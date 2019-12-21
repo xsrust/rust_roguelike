6e2bed94ac794bbbfd4f85584b6765996394deef
Sam Rust <xsrust@gmail.com>
2019-12-21 15:09:36 +0000

    This time with walls

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,18 @@ use tcod::console::*;
 /// Actual size of the window
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
+/// size of the map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
 /// Other settings
 const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+/// Colors
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
 
 /*********  STRUCTURES  *********/
 struct Tcod {
@@ -16,11 +26,35 @@ struct Tcod {
 
 /// This is a generic object: the player, a monster, an item, the stairs...
 /// It's always represented by a charactor on the screen.
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
     char: char,
     color: Color,
+}
+
+/// A tile of the map and it's prooerties
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+/// a list of list of tiles, to represent the map
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+/// A rectangle on the map, used to characterise a room.
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
 }
 
 fn main() {
@@ -42,26 +76,15 @@ fn main() {
 
     // List of objects in the game, currently player, npc
     let mut objects = [player, npc];
+    let game = Game { map: make_map()};
 
     while !tcod.root.window_closed() {
-        tcod.con.clear(); // clean console
-        for object in &objects {
-            object.draw(&mut tcod.con);
-        }
-        tcod.root.flush(); // clean root console to write to
-                           // bit the contents of "con" onto the root console and present it
-        blit(
-            &tcod.con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
+        // handle the updating of the view port
+        render_all(&mut tcod, &game, &objects);
+
         // Key handleing w/ exit
         let player = &mut objects[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(&mut tcod, player, &game);
         if exit {
             break;
         }
@@ -75,9 +98,11 @@ impl Object {
     }
 
     /// move by the given amount
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
     /// set the color and then draw the character that represents this object at it's
@@ -88,8 +113,68 @@ impl Object {
     }
 }
 
+/// Functions for the tile structure
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+
+
+/// render all of the things
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+    tcod.con.clear(); // clean console
+    // Draw all of our objects on the screen
+    for object in objects {
+        object.draw(&mut tcod.con);
+    }
+    // Draw all our tiles onto the screen
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con.set_char_background(x,y,COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con.set_char_background(x,y,COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+    tcod.root.flush(); // clean root console to write to
+    // blit the contents of "con" onto the root console and present it
+    blit(
+        &tcod.con,
+        (0, 0),
+        (SCREEN_WIDTH, SCREEN_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
+}
+
+/// Create our Map object
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+    map
+}
+
 /// Handle Key inputs from the user
-fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+fn handle_keys(tcod: &mut Tcod, player: &mut Object, game: &Game) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -108,10 +193,10 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
         // Exit the game when the escape key is pressed
         Key { code: Escape, .. } => return true,
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
 
         _ => {}
     }
