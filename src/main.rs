b3d589e0a211f6a1fdef7b6d17e3fd2243659583
Sam Rust <xsrust@gmail.com>
2019-12-21 15:49:42 +0000

    Now with a procedurally generated dungeon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
@@ -11,6 +12,10 @@ const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 /// Other settings
 const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+/// Room settings
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 /// Colors
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color {
@@ -77,7 +82,9 @@ fn main() {
 
     // List of objects in the game, currently player, npc
     let mut objects = [player, npc];
-    let game = Game { map: make_map() };
+    let game = Game {
+        map: make_map(&mut objects[0]),
+    };
 
     while !tcod.root.window_closed() {
         // handle the updating of the view port
@@ -141,7 +148,23 @@ impl Rect {
             y2: y + h,
         }
     }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        // returns tru if this rectangle intersects with another one
+        (self.x1 <= other.x2)
+            && (self.x2 > other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
 }
+
+/********** GENERIC FUNCTIONS ***********/
 
 /// render all of the things
 fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
@@ -177,15 +200,62 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
 }
 
 /// Create our Map object
-fn make_map() -> Map {
+fn make_map(player: &mut Object) -> Map {
     // fill map with "unblocked" tiles
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
     // create the rooms
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
-    create_h_tunnel(25, 55, 23, &mut map);
+    let mut rooms = vec![];
+    for _ in 0..MAX_ROOMS {
+        // random width and height
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        // random positions without going off the bounds of the map
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        // run through and check if any others intersect
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+
+        if !failed {
+            // valid room with no intersections
+
+            // "paint" it to the map's tiles
+            create_room(new_room, &mut map);
+
+            // center coordinates fo the new room, will be useful later
+            let (new_x, new_y) = new_room.center();
+
+            if rooms.is_empty() {
+                // this is the first room, where the player starts at
+                player.x = new_x;
+                player.y = new_y;
+            } else {
+                // all rooms after the first:
+                // connect it to the previous with a tunnel
+
+                // center coords of the previous room
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // toss a coin
+                if rand::random() {
+                    // first move horizontally, then vert
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    // first move vert, then horiz
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+
+            // append the new room to the list
+            rooms.push(new_room);
+        }
+    }
 
     map
 }
