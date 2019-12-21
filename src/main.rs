bf82078d4b05e28c1d522b2ad58f37cc45fbdd71
Sam Rust <xsrust@gmail.com>
2019-12-21 14:21:49 +0000

    player is an object, so is EVERYTHING

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,15 @@ struct Tcod {
     con: Offscreen,
 }
 
+/// This is a generic object: the player, a monster, an item, the stairs...
+/// It's always represented by a charactor on the screen.
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
 fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
@@ -28,16 +37,19 @@ fn main() {
     let mut tcod = Tcod { root, con };
 
     // Initilize Player Properties
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    // List of objects in the game, currently player, npc
+    let mut objects = [player, npc];
 
     while !tcod.root.window_closed() {
-        tcod.con.set_default_foreground(WHITE); // set drawl color
         tcod.con.clear(); // clean console
-        tcod.con
-            .put_char(player_x, player_y, '@', BackgroundFlag::None); // Draw PC
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
         tcod.root.flush(); // clean root console to write to
-        // bit the contents of "con" onto the root console and present it
+                           // bit the contents of "con" onto the root console and present it
         blit(
             &tcod.con,
             (0, 0),
@@ -48,15 +60,36 @@ fn main() {
             1.0,
         );
         // Key handleing w/ exit
-        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
         if exit {
             break;
         }
     }
 }
 
+/// Fucntions for the object structure
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    /// move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    /// set the color and then draw the character that represents this object at it's
+    /// position on the screen
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
 /// Handle Key inputs from the user
-fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -75,10 +108,10 @@ fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool 
         // Exit the game when the escape key is pressed
         Key { code: Escape, .. } => return true,
         // movement keys
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         _ => {}
     }
